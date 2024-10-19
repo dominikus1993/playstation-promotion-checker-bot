6e9c92fd511efee620133cab2045911d7f5a7c4f
Dominik.Kotecki <dominikus1910@gmail.com>
2024-10-19 15:10:56 +0000

    add parser interface

--- pkg/parsers/psn.go
+++ pkg/parsers/psn.go
@@ -1,4 +1,11 @@
 package parsers
 
+import (
+	"context"
+
+	"github.com/dominikus1993/playstation-promotion-checker-bot/pkg/data"
+)
+
 type PlaystationNetworkDataParser interface {
+	Provide(ctx context.Context) <-chan data.PlaystationStoreGame
 }
