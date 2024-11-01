3c04fbe794350ec0b73716f57dc46320d4bf4bc4
Dominik.Kotecki <dominikus1910@gmail.com>
2024-11-01 15:27:00 +0000

    fix

--- main.go
+++ main.go
@@ -1,8 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/dominikus1993/playstation-promotion-checker-bot/cmd"
+	"github.com/urfave/cli/v2"
+	_ "go.uber.org/automaxprocs"
+)
 
 func main() {
-	basePath := "https://store.playstation.com/pl-pl/category/83a687fe-bed7-448c-909f-310e74a71b39"
-	fmt.Println(basePath)
+	app := &cli.App{
+		Name:  "xbox-promotion-bot",
+		Usage: "parse xbox game promotions",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "webhooktoken",
+				Usage:    "discord webhook token",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "webhookid",
+				Usage:    "discord webhhook id",
+				Required: true,
+			},
+			&cli.Float64Flag{
+				Name:     "pricePromotionPercentage",
+				Aliases:  []string{"ppp"},
+				Usage:    "minimum promotion percentage",
+				Value:    50,
+				Required: false,
+			},
+			&cli.StringFlag{
+				Name:     "mongo-connection",
+				Usage:    "mongodb connection string",
+				Required: true,
+			},
+		},
+		Action: cmd.XboxGamePromotionParser,
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
